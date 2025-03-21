cf233db3c2e5cb5611269d2696d7177803c7a6ad
alfredo <alfredo@debian>
2025-03-21 23:37:21 +0100

    Remove case statement

--- Brightness.sh
+++ Brightness.sh
@@ -1,41 +1,40 @@
 #!/bin/sh
+
 clear
-echo '################################################'
-echo '#                                              #'
-echo '#      Based on bash 'xrandr' command            #'
-echo '#                                              #'
-echo '#    Script for regulate monitor brightness    #'
-echo '#                                              #'
-echo '################################################'
-echo ''
-echo ' Output avaiable are: '
-echo ''
-echo ' 1)LVDS  2)VGA  3)HDMI  4)DP '
-echo ''
-read -p ' Insert the number of graphic port: ' ANSWER
-echo ''
-read -p ' Insert a value to 0.1 (min) at 1 (max): ' BRING
-while :
-do
-     case $ANSWER in 
-        1)
-          xrandr --output LVDS-1 --brightness $BRING
-          break;;
-        2)
-          xrandr --output VGA-1 --brightness $BRING
-          break;;
-        3)
-          xrandr --output HDMI-1 --brightness $BRING
-          break;;
-        4)
-          xrandr --output DP-1 --brightness $BRING
-          break;;
-        *)
-          echo 'Invalid graphic port,$BRING is a non correct option number';;
-      esac
+
+echo -e "\n =========================================="
+echo -e ""    Script for regulate monitor brightness
+echo -e ""      	Based on xrandr command
+echo -e " ==========================================\n"
+
+MIN_BRIGHTNESS="0.1"
+MAX_BRIGHTNESS="3"
+
+monitors=($(xrandr | awk '/ connected / {print $1}'))
+
+AMOUNT_MONITORS=${#monitors[@]}
+
+echo -e " Monitor connected:"
+for ((monitor=0; monitor<AMOUNT_MONITORS; monitor++)); do
+    echo " Monitor $((monitor+1)) --> ${monitors[$monitor]}"
 done
 echo ""
-echo " Brightness monitor regulated!" # working only with ""
-echo ""
-  	
-	
+
+while true; do
+	read -p " Choose connected monitors: " MONITOR_CHOCE
+	read -p " Insert a value to $MIN_BRIGHTNESS (min) at $MAX_BRIGHTNESS (max): " BRING
+	MONITOR_CHOCE_ON_ARRAY=$(($MONITOR_CHOCE - 1))
+	INTEGER_BRING=$(echo "$BRING * 10" | bc)
+	if (( MONITOR_CHOCE_ON_ARRAY <= AMOUNT_MONITORS && MONITOR_CHOCE_ON_ARRAY >= 0 )); then
+		#if (( $(echo "$brightness_value >= $MIN_BRIGHTNESS" | bc -l) && $(echo "$brightness_value <= $MAX_BRIGHTNESS" | bc -l) )); then
+		if (("$INTEGER_BRING" -le "$MAX_BRIGHTNESS") && ("$INTEGER_BRING" -gt 0 )); then
+			xrandr --output ${monitors[$MONITOR_CHOCE_ON_ARRAY]} --brightness "$BRING"
+			echo -e "\n Brightness monitor regulated!"
+			exit 0
+		else
+			echo " Brightness value is not correct, retry"
+		fi
+	else
+		echo " Number monitor $MONITOR_CHOCE is not valid, retry"
+	fi
+done
